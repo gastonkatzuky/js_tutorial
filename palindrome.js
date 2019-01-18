36d0dac522cb5a7612c793f3ce8e6df92163a80c
Gaston Katzuky <iatzkypedro@gmail.com>
2019-01-17 21:50:50 -0300

    Finish section 7.2

--- palindrome.js
+++ palindrome.js
@@ -13,15 +13,31 @@ function palindrome(string) {
 function Phrase(content) {
   this.content = content;
 
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return  this.content.toLowerCase();
+  }
   // Returns true for a palindrome, false otherwise.
   this.palindrome = function palindrome(string) {
-    let processedContent = this.content.toLowerCase();
-    return processedContent === reverse(processedContent);
+    return this.processedContent() === reverse(this.processedContent());
   }
 
     // Makes the phrase LOUDER.
   this.louder = function() {
     return this.content.toUpperCase();
-  };
+  }
+}
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.translation.toLowerCase();
+  }
 
 }
+
+TranslatedPhrase.prototype = new Phrase();
